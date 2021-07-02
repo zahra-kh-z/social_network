f04712b8e82c87af11e630398aa483088869286a
Zahra_kh <56346712+zahra-kh-z@users.noreply.github.com>
2021-07-02 13:46:24 +0430

    Update admin.py

--- book/admin.py
+++ book/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 # from book.models import Book
-from .models import Book, Author
+from .models import Book, Comment, Like
 
 admin.site.register(Book)
-admin.site.register(Author)
+admin.site.register(Comment)
+admin.site.register(Like)
 
+# admin.site.register(Author)
 # @admin.register(Book)
 # class BookAdmin(admin.ModelAdmin):
 #     list_display = ['id', 'name']
+
