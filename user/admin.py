549c9c897cdcac30b8db81ee41c3cbd650948135
Zahra_kh <56346712+zahra-kh-z@users.noreply.github.com>
2021-07-02 13:48:10 +0430

    Update admin.py

--- user/admin.py
+++ user/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
+from user.models import User
+from user.models import Relationship
 
 # Register your models here.
-from user.models import User
-
-
 # admin.site.register(User)
+admin.site.register(Relationship)
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username']
+
