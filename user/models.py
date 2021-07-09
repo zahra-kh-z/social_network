a619e598eaf4f5452072cdd262930f2a6e835cf5
Zahra <zahra.kh2005@gmail.com>
2021-07-09 17:38:41 +0430

    add get_friends,get_books functions

--- user/models.py
+++ user/models.py
@@ -22,7 +22,7 @@ class User(models.Model):
     website = models.URLField('website')
     email = models.EmailField('email')
     register_date = models.DateTimeField('register date', auto_now_add=True)
-    update_date = models.DateTimeField('update date')
+    update_date = models.DateTimeField('update date', auto_now=True)
     credit = models.IntegerField('credit', default=20)
     friends = models.ManyToManyField("User", blank=True, related_name="friend")
 
@@ -35,7 +35,7 @@ class User(models.Model):
         return f'{self.username} registered at {self.register_date}'
 
     def delete(self):
-        """This method deleted the object/user """
+        """This method deleted the object/user"""
         deleted_obj = f'{self.first_name}_{self.last_name} object/user deleted'
         self.delete()
         return deleted_obj
@@ -45,9 +45,25 @@ class User(models.Model):
         self.credit += amount
         self.save()
 
+    def get_friends(self):
+        """return a list of user friend"""
+        return self.friends.all()
+
+    def get_friends_no(self):
+        """return count of friend"""
+        return self.friends.all().count()
+
+    def get_books(self):
+        """get all book of user"""
+        return self.user.all()
+
+    def get_books_no(self):
+        """get count all book of user"""
+        return self.user.all().count()
+
 
 class Relationship(models.Model):
-    """This method defined a relationship for a user """
+    """This method defined a relationship for a user"""
     STATUS_CHOICES = [('A', 'accepted'), ('R', 'requested'), ('N', 'name')]
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
@@ -57,3 +73,28 @@ class Relationship(models.Model):
 
     def __str__(self):
         return f'{self.sender}, {self.receiver}, {self.status}'
+
+# SocialNetworkBook command shell
+# C:\1_venv_django\SocialNetworkBook>..\my_venv_django\Scripts\activate
+# (my_venv_django) C:\1_venv_django\SocialNetworkBook>python manage.py shell
+# >>> from user.models import User
+# >>> parisa=User(first_name="parisa",last_name="etemadi",username="parisa74103",gender="F",
+# phone_number="09010488477",biography="MSc",country="iran",website="parisa.ir",email="parisa@gmail.com")
+# >>> parisa.save()
+# >>> sara=User(first_name="sara",last_name="ghaneei",username="sara74103",gender="F",phone_number="090100000",
+# biography="MSc",country="iran",website="sara.ir",email="sara@gmail.com")
+# >>> sara.save()
+# >>> zahra=User(first_name="zahra",last_name="matinfar",username="zahra74103",gender="F",phone_number="090111111",
+# biography="MSc",country="iran",website="zahra.ir",email="zahra@gmail.com")
+# >>> zahra.save()
+# >>> elnaz=User(first_name="elnaz",last_name="etemadi",username="elnaz74103",gender="F",phone_number="09010488477",
+# biography="MSc",country="iran",website="elnaz.ir",email="elnaz@gmail.com")
+# >>> elnaz.save()
+# >>> sara.friends.add(zahra)
+# >>> sara.friends.all()
+# <QuerySet [<User: zahra74103 registered at 2021-07-09 12:58:39.261273+00:00>]>
+# >>> sara.friends.add(parisa)
+# >>> sara.friends.all()
+# <QuerySet [<User: parisa74103 registered at 2021-07-09 12:58:03.315138+00:00>, <User: zahra74103 registered
+# at 2021-07-09 12:58:39.261273+00:00>]>
+# >>>
